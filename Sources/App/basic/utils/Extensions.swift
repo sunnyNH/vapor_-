b239c6a1a27e76ee41db0f9f5bb632431ffef97c
zhzsunny <157666848+zhzsunny@users.noreply.github.com>
2024-12-08 12:42:01 +0800

    ns

--- Sources/App/basic/utils/Extensions.swift
+++ Sources/App/basic/utils/Extensions.swift
@@ -8,13 +8,3 @@ extension Abort {
         return Abort(HTTPResponseStatus.badRequest, reason: message)
     }
 }
-
-// MARK: - String 扩展
-extension String {
-    /// 验证是否为手机号
-    var isPhone: Bool {
-        let pattern = "^1[3-9]\\d{9}$"
-        let predicate = NSPredicate(format: "SELF MATCHES %@", pattern)
-        return predicate.evaluate(with: self)
-    }
-}
